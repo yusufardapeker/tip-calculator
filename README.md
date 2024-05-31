a16b6f494ebfe723ff0af791ee54a9f8cdf9c468
Yusuf Arda Peker <yusufardapkr@gmail.com>
2024-05-31 09:46:08 +0300

    Initialize repository

---
+++ README.md
@@ -0,0 +1,12 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX).
+
+[The Website](https://tip-calculator5039.netlify.app/)
+
+### Built with
+
+- SASS
+- Mobile-first workflow
+
+I don't have the figma file so it is not pixel perfect. I have tried to make similar as same as possible. The website is responsive only 1440px and 375px screen sizes. I'll be glad if you give me suggestion about mistakes, lackings and best practices and how can I fix it.
---
+++ script.js
@@ -0,0 +1,77 @@
+const billAmountInput = document.querySelector(".bill-amount-input");
+const selectTipButtons = document.querySelectorAll(".bill-select-button");
+const billPeopleInput = document.querySelector(".bill-people-number-input");
+const errorMessages = document.querySelectorAll(".error-msg");
+
+const tipResultAmount = document.querySelector(".tip-result-number.amount");
+const tipResultTotal = document.querySelector(".tip-result-number.total");
+
+const resetBtn = document.querySelector(".reset-btn");
+
+const getSelectedRate = () => {
+	selectTipButtons.forEach((button, index, array) => {
+		button.addEventListener("click", (e) => {
+			array.forEach((element) => element.classList.remove("selected"));
+			e.target.classList.add("selected");
+
+			let selectedButtonVal = parseInt(e.target.textContent);
+			calculate(selectedButtonVal);
+
+			if (array[5] === array[index]) {
+				array[5].addEventListener("input", (e) => {
+					selectedButtonVal = parseInt(e.target.value);
+
+					calculate(selectedButtonVal);
+				});
+			}
+		});
+	});
+};
+
+getSelectedRate();
+
+const inputs = [billAmountInput, billPeopleInput];
+
+const calculate = (buttonRate = 15) => {
+	inputs.forEach((input, index, array) => {
+		input.addEventListener("input", () => {
+			let amountVal = parseInt(array[0].value);
+			let personVal = parseInt(array[1].value);
+
+			isCorrect(amountVal, personVal);
+
+			let tipCost = (amountVal * buttonRate) / 100;
+
+			if (amountVal > 0 && personVal > 0) {
+				tipResultAmount.textContent = (tipCost / personVal).toFixed(2);
+				tipResultTotal.textContent = ((amountVal + tipCost) / personVal).toFixed(2);
+				resetBtn.disabled = false;
+			}
+		});
+	});
+};
+
+const isCorrect = (...args) => {
+	const argumants = [...args];
+
+	argumants.forEach((argumant, index) => {
+		if (argumant === 0) {
+			inputs[index].classList.add("error");
+			errorMessages[index].style.display = "block";
+		} else {
+			inputs[index].classList.remove("error");
+			errorMessages[index].style.display = "none";
+		}
+	});
+};
+
+resetBtn.addEventListener("click", () => {
+	billAmountInput.value = "";
+	selectTipButtons.forEach((button) => button.classList.remove("selected"));
+	billPeopleInput.value = "";
+	tipResultAmount.textContent = "$0.00";
+	tipResultTotal.textContent = "$0.00";
+	resetBtn.disabled = true;
+});
+
+calculate();
